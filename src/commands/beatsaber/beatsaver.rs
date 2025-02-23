ba1faccf7fd3132c600720576e723ad02d7e5e22
pochama <mercworld@pocha.moe>
2025-02-22 21:49:12 -0500

    feat: /bsr download link

--- src/commands/beatsaber/beatsaver.rs
+++ src/commands/beatsaber/beatsaver.rs
@@ -1,34 +1,70 @@
+use crate::{
+    services::beatsaver::{api::get_map_data, map::BSMap},
+    Context, Error,
+};
 use log::info;
-use poise::{self, serenity_prelude::CreateEmbed, CreateReply};
-use crate::{services::beatsaver::{api::get_map_data, map::BSMap}, Context, Error};
+use poise::{
+    self,
+    serenity_prelude::{
+        CreateActionRow, CreateButton, CreateEmbed, CreateSelectMenu, CreateSelectMenuKind, CreateSelectMenuOption
+    },
+    CreateReply,
+};
 
-fn create_bsr_embed(map_info: Result<BSMap, Box<dyn std::error::Error>>, code: String) -> CreateEmbed {
+/// Creates the general map info embed.
+fn create_map_info_embed(map: &BSMap, code: String) -> CreateEmbed {
     let embed: CreateEmbed;
 
-    match map_info {
-        Ok(map) => {
-            embed = CreateEmbed::new()
-                .title(map.name)
-                .url(format!("https://beatsaver.com/maps/{}", code))
-                .description(map.description)
-                .field("Mapper(s)", map.metadata.level_author_name, false)
-                .field("Artist(s)", map.metadata.song_author_name, false)
-                .fields([
-                    ("BPM", &map.metadata.bpm.to_string(), true),
-                    ("Length", &format!("{}:{:0>2}", map.metadata.duration % 60, (map.metadata.duration / 60) % 60), true),
-                    ("Rating", &format!("▲ {} / ▼ {} ({}%)", map.stats.upvotes, map.stats.downvotes, map.stats.score * 100.0), true),
-                ])
-                .thumbnail(&map.versions[0].cover_url)
-                .timestamp(map.uploaded);
-        }
-        Err(error) => {
-            embed = CreateEmbed::new()
-                .title("Error")
-                .description(error.to_string());
-        }
-    }
+    embed = CreateEmbed::new()
+        .title(&map.name)
+        .url(format!("https://beatsaver.com/maps/{}", code))
+        .description(&map.description)
+        .field("Mapper(s)", &map.metadata.level_author_name, false)
+        .field("Artist(s)", &map.metadata.song_author_name, false)
+        .fields([
+            ("BPM", &map.metadata.bpm.to_string(), true),
+            (
+                "Length",
+                &format!(
+                    "{}:{:0>2}",
+                    map.metadata.duration % 60,
+                    (map.metadata.duration / 60) % 60
+                ),
+                true,
+            ),
+            (
+                "Rating",
+                &format!(
+                    "▲ {} / ▼ {} ({}%)",
+                    map.stats.upvotes,
+                    map.stats.downvotes,
+                    map.stats.score * 100.0
+                ),
+                true,
+            ),
+        ])
+        .thumbnail(&map.versions[0].cover_url)
+        .timestamp(map.uploaded);
 
     embed
+}
+
+/// Creates the options menu for the available difficulties.
+fn get_map_diffs(map: &BSMap) -> Vec<CreateSelectMenuOption> {
+    let map_diffs: Vec<CreateSelectMenuOption>;
+
+    map_diffs = map.versions[0]
+        .diffs
+        .iter()
+        .map(|diff| {
+            CreateSelectMenuOption::new(
+                format!("{} ({})", diff.difficulty, diff.characteristic),
+                format!("{}{}", diff.characteristic, diff.difficulty),
+            )
+        })
+        .collect();
+
+    map_diffs
 }
 
 /// Searches a Beat Saber custom map from BeatSaver.   
@@ -38,12 +74,43 @@ pub async fn bsr(
     #[description = "The beatmap code (up to 5 alphanumeric characters)"] code: String,
 ) -> Result<(), Error> {
     info!("/bsr used with code {}", &code);
-    let map_info = get_map_data(&code).await;
+    let embed: CreateEmbed;
+    let diffs: Vec<CreateSelectMenuOption>;
+    let builder: CreateReply;
 
-    let embed = create_bsr_embed(map_info, code); 
-    let builder = CreateReply::default().embed(embed);
+    match get_map_data(&code).await {
+        Ok(map) => {
+            embed = create_map_info_embed(&map, code);
+            diffs = get_map_diffs(&map);
+
+            builder =
+                CreateReply::default()
+                    .embed(embed)
+                    .components(vec![
+                        CreateActionRow::SelectMenu(
+                            CreateSelectMenu::new(
+                                "diffsel",
+                                CreateSelectMenuKind::String { options: diffs },
+                            )
+                            .placeholder("Select Difficulty"),
+                        ),
+                        CreateActionRow::Buttons(vec![
+                            CreateButton::new_link(&map.versions[0].download_url)
+                                .label("Download map")
+                                .emoji('⬇')
+                            ]
+                        )
+                    ]);
+        }
+        Err(err) => {
+            embed = CreateEmbed::new()
+                .title("Error!")
+                .description(err.to_string());
+
+            builder = CreateReply::default().embed(embed);
+        }
+    }
 
     ctx.send(builder).await?;
-
     Ok(())
 }
