fd905efce5f68b16f4d468dfa10d2d7b7af6b137
ポッチャマ <mercworld@pocha.moe>
2025-03-04 21:39:55 -0500

    ci: add appspec.yml
    ok will it work now

--- codedeploy/hooks/ApplicationStart.sh
+++ codedeploy/hooks/ApplicationStart.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+set -euox pipefail
+
+cd /var/lib/codedeploy-apps/mafuyu
+
+if ! docker compose up --detach --wait --wait-timeout 120 ; then
+    docker compose logs
+    exit 1
+fi
--- codedeploy/hooks/ApplicationStop.sh
+++ codedeploy/hooks/ApplicationStop.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+set -euox pipefail
+
+cd /var/lib/codedeploy-apps/mafuyu
+
+docker compose down
